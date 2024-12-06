082ab66a41829c7f99fee33b1ad3230f6c2ca992
Sulaiman Zahir <sulaimanzahir94@outlook.com>
2024-12-06 21:15:24 +0300

    Add project files.

---
+++ Carseer/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Carseer.Models;
+using Carseer.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+
+namespace Carseer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly VehicleService _vehicleService;
+
+        public HomeController(ILogger<HomeController> logger, VehicleService vehicleService)
+        {
+            _logger = logger;
+            _vehicleService = vehicleService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var makes = await _vehicleService.GetAllMakes();
+            ViewBag.Makes = makes.Select(m => new SelectListItem(m.Make_Name, m.Make_ID.ToString())).ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(int selectedMakeId, int year, string selectedVehicleType)
+        {
+            var vehicleTypes = await _vehicleService.GetVehicleTypesForMake(selectedMakeId);
+            ViewBag.VehicleTypes = vehicleTypes.Select(v => new SelectListItem(v.VehicleTypeName, v.VehicleTypeName)).ToList();
+
+            var models = await _vehicleService.GetModelsForMakeAndYear(selectedMakeId, year, selectedVehicleType);
+            return View(models);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Carseer/Services/VehicleService.cs
@@ -0,0 +1,67 @@
+﻿namespace Carseer.Services
+{
+    public class VehicleService
+    {
+        private readonly HttpClient _httpClient;
+
+        public VehicleService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Make>> GetAllMakes()
+        {
+            var response = await _httpClient.GetAsync("https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=json");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadFromJsonAsync<ApiResponse<Make>>();
+                return data.Results;
+            }
+            return new List<Make>();
+        }
+
+        public async Task<List<VehicleType>> GetVehicleTypesForMake(int makeId)
+        {
+            var response = await _httpClient.GetAsync($"https://vpic.nhtsa.dot.gov/api/vehicles/GetVehicleTypesForMakeId/{makeId}?format=json");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadFromJsonAsync<ApiResponse<VehicleType>>();
+                return data.Results;
+            }
+            return new List<VehicleType>();
+        }
+
+        public async Task<List<Model>> GetModelsForMakeAndYear(int makeId, int year, string vehicleType)
+        {
+            var response = await _httpClient.GetAsync($"https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/{makeId}/modelyear/{year}?format=json");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadFromJsonAsync<ApiResponse<Model>>();
+                return data.Results.Where(m => m.VehicleType == vehicleType).ToList();
+            }
+            return new List<Model>();
+        }
+    }
+
+    public class ApiResponse<T>
+    {
+        public List<T> Results { get; set; }
+    }
+
+    public class Make 
+    { 
+        public int Make_ID { get; set; } 
+        public string Make_Name { get; set; } 
+    }
+    public class VehicleType 
+    { 
+        public string VehicleTypeName { get; set; } 
+    }
+    public class Model 
+    { 
+        public string Model_Name { get; set; } 
+        public string VehicleType { get; set; } 
+    }
+
+
+}
